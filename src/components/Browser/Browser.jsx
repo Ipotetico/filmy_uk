8ce30c3ada7b3677f3a63e64f21514ee1138bb6c
Ipotetico <zbro72@o2.pl>
2022-07-31 17:54:06 +0200

    31-07-22___01

--- src/components/Browser/Browser.jsx
+++ src/components/Browser/Browser.jsx
@@ -4,18 +4,22 @@ import Info from './Info/Info';
 import MoviesList from './MoviesList/MoviesList';
 import Footer from './Footer/Footer';
 import MenuHamburg from './Menu/MenuHamburg/MenuHamburg';
+import CookieConsent from "./CookiesConsent/CookiesConsent";
 
 import './Browser.scss';
 import Login from './Login/Login';
 
+
 const Browser = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [open, setOpen] = useState(false);
+  const [cookiesConsent, setCookiesConsent] = useState(false)
 
   console.log(isLogged);
 
   return (
     <div className='browser_container'>
+        <CookieConsent cookiesConsent={cookiesConsent} setCookiesConsent={setCookiesConsent}/>
       <Menu open={open} setOpen={setOpen} />
       <Info />
       {isLogged ? <MoviesList /> : <Login setIsLogged={setIsLogged} />}
